19300e09203834e7c1d4df7eee52511f91be22f4
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-11-06 11:32:54 +0800

    beres

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,13 +21,7 @@ import HomePage from '@/components/layouts/Homepage/page'
 
 const index = () => {
 
-  const images = [
-    'https://images.unsplash.com/photo-1682687981630-cefe9cd73072?auto=format&fit=crop&q=80&w=2671&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1682687981630-cefe9cd73072?auto=format&fit=crop&q=80&w=2671&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1682687981630-cefe9cd73072?auto=format&fit=crop&q=80&w=2671&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1682687981630-cefe9cd73072?auto=format&fit=crop&q=80&w=2671&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    // Add more image URLs as needed
-  ];
+  
   return (
     <div>
       <HomePage/>
