d3245d0395ab789c3ba57582027d0449d433d5f7
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-11-06 20:35:15 +0800

    PAge manage

--- src/components/layouts/ImageCarosel/page.jsx
+++ src/components/layouts/ImageCarosel/page.jsx
@@ -9,9 +9,30 @@ const ImageCarousel = () => {
   ];
 
   const [activeIndex, setActiveIndex] = useState(0);
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
 
   const handleDotClick = (index) => {
     setActiveIndex(index);
+  };
+
+  const handleSwipe = () => {
+    if (touchEndX - touchStartX > 50) {
+      // Swipe right
+      setActiveIndex((prevIndex) => (prevIndex === 0 ? imageUrls.length - 1 : prevIndex - 1));
+    } else if (touchStartX - touchEndX > 50) {
+      // Swipe left
+      setActiveIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+    }
+  };
+
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchEnd = (e) => {
+    setTouchEndX(e.changedTouches[0].clientX);
+    handleSwipe();
   };
 
   useEffect(() => {
@@ -26,7 +47,11 @@ const ImageCarousel = () => {
 
   return (
     <div>
-      <div className="w-1716 h-793 rounded-lg overflow-hidden mx-24">
+      <div
+        className="w-1716 h-793 rounded-lg overflow-hidden mx-24"
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
         {imageUrls.map((url, index) => (
           <img
             key={index}
@@ -44,8 +69,8 @@ const ImageCarousel = () => {
         {imageUrls.map((_, index) => (
           <BsDot
             key={index}
-            className={` text-gray-400 text-5xl cursor-pointer ${
-              index === activeIndex ? " text-gray-600" : ""
+            className={`text-gray-400 text-5xl cursor-pointer ${
+              index === activeIndex ? "text-gray-600" : ""
             }`}
             onClick={() => handleDotClick(index)}
           />
